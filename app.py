417f89d9f176ffe01da4e8ac06fee1336e8dc13c
G M Ashikur Rahman <48297994+gmashik@users.noreply.github.com>
2021-07-27 11:54:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,33 +33,5 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/get_end_predictions', methods=['GET','POST'])
-def get_prediction_eos():
-    try:
-        input_text = ' '.join(request.json['input_text'].split())
-        #input_text += ' <mask>'
-        top_k = request.json['top_k']
-        m=recommendation(input_text,model,tokenizer,int(top_k))
-        res = {'bert': m}
-        return app.response_class(response=json.dumps(res), status=200, mimetype='application/json')
-    except Exception as error:
-        err = str(error)
-        print(err)
-        return app.response_class(response=json.dumps(err), status=500, mimetype='application/json')
-
-
-# @app.route('/get_mask_predictions', methods=['post'])
-# def get_prediction_mask():
-#     try:
-#         input_text = ' '.join(request.json['input_text'].split())
-#         top_k = request.json['top_k']
-#         res = main.get_all_predictions(input_text, top_clean=int(top_k))
-#         return app.response_class(response=json.dumps(res), status=200, mimetype='application/json')
-#     except Exception as error:
-#         err = str(error)
-#         print(err)
-#         return app.response_class(response=json.dumps(err), status=500, mimetype='application/json')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
